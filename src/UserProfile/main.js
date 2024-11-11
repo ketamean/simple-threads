dd55945692af05f70a630d9cd7e41fba50a8416b
Hai Do <dodinhhai29904@gmail.com>
2024-11-11 17:43:00 +0700

    add share modal to user profile add logic to share modal, unfollow modal and friend board

--- src/UserProfile/main.js
+++ src/UserProfile/main.js
@@ -1,6 +1,15 @@
-let love = document.querySelectorAll('.love');
-let loveIcon = document.querySelectorAll('.iconoir-heart');
-let likeNum = document.querySelectorAll('.like-number');
+const love = document.querySelectorAll('.love');
+const loveIcon = document.querySelectorAll('.iconoir-heart');
+const likeNum = document.querySelectorAll('.like-number');
+const followStatus = document.querySelectorAll('.follow-status');
+const unfollowModal = document.querySelector('.unfollow-modal');
+const unfollowCard = document.querySelector('.unfollow-card');
+const followersPreview = document.querySelector('.followers-preview');
+const followBoard = document.querySelector('.profile-follow-board');
+const cancelUnfollow = document.querySelector('.cancel');
+const actionRepost = document.querySelectorAll('.action.repost');
+const shareModal = document.querySelector('.share-modal');
+
 for (let i = 0; i < love.length; i++) {
     love[i].addEventListener('click', () => {
         if (loveIcon[i].classList.contains('iconoir-heart-solid')) {
@@ -16,3 +25,54 @@ for (let i = 0; i < love.length; i++) {
         }
     });
 }
+
+
+followersPreview.addEventListener('click', () => {
+    followBoard.classList.toggle('active');
+    if (followBoard.classList.contains('active')) {
+        document.body.style.overflow = 'hidden';
+    }
+});
+
+document.addEventListener('click', (e) => {
+    // if both the followersPreview and followBoard does not have the clicked target, close the followBoard
+    if (!followersPreview.contains(e.target) && !followBoard.contains(e.target) && !unfollowModal.contains(e.target)
+        && !unfollowCard.contains(e.target) && !shareModal.contains(e.target) && !actionRepost.contains(e.target)
+    ) {
+        followBoard.classList.remove('active');
+        document.body.style.overflow = 'auto';
+    }
+});
+
+unfollowModal.addEventListener('click', (e) => {
+    if (unfollowModal.contains(e.target) && !unfollowCard.contains(e.target)) {
+        unfollowModal.classList.remove('active');
+        document.body.style.overflow = 'auto';
+    }
+});
+
+followStatus.forEach((status) => {
+    status.addEventListener('click', () => {
+        unfollowModal.classList.add('active');
+        document.body.style.overflow = 'hidden';
+    });
+});
+
+cancelUnfollow.addEventListener('click', () => {
+    unfollowModal.classList.remove('active');
+    document.body.style.overflow = 'auto';
+});
+
+actionRepost.forEach((repost) => {
+    repost.addEventListener('click', () => {
+        shareModal.classList.add('active');
+        document.body.style.overflow = 'hidden';
+    });
+});
+
+shareModal.addEventListener('click', (e) => {
+    if (shareModal.contains(e.target)) {
+        shareModal.classList.remove('active');
+        document.body.style.overflow = 'auto';
+    }
+});
