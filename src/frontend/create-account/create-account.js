13e60d175d1b77431c5991060afdc9c905500d25
ketamean <truongthanhtoan2003@gmail.com>
2024-11-13 11:28:24 +0700

    Password check when create account

--- src/frontend/create-account/create-account.js
+++ src/frontend/create-account/create-account.js
@@ -1,23 +1,8 @@
 const pswInput = document.querySelector('#password');
 const pswStrengthBar = document.querySelector('#password-strength-bar')
 const cfPswInput = document.querySelector('#cf-password')
-const pswMatchBar = document.querySelector('#password-match-bar')
-
-function containsSpecialChar(str) {
-    return str.match(/[!"#$%&'()*+,\-./:;<=>?@\[\\\]^_`{|}~]/) !== null
-}
-
-function containsUppercaseLetter(str) {
-    return str.match(/[A-Z]/)
-}
-
-function containsLowsercaseLetter(str) {
-    return str.match(/[a-z]/)
-}
-
-function containsNumber(str) {
-    return str.match(/[0-9]/)
-}
+const pswMatchSymbol = document.querySelector('#password-match')
+import { passwordScoring } from '../pswcheck.js'
 
 pswInput.addEventListener(
     'input',
@@ -35,23 +20,19 @@ pswInput.addEventListener(
             - score = 3 4       average
             - score = 5         strong
         */
-        let score = 0
+        
         let str = pswInput.value
 
 
         if (str.length === 0) {
-            pswStrengthBar.style.display = 'none'
+            pswStrengthBar.style.visibility = 'hidden'
             return
-        } else pswStrengthBar.style.display = 'block'
+        } else pswStrengthBar.style.visibility = 'visible'
         // remove password matched notation
         checkPswMatch()
 
         // check password strength
-        if (str.length >= 8) score++    
-        if ( containsLowsercaseLetter(str) ) score++
-        if ( containsUppercaseLetter(str) ) score++
-        if ( containsSpecialChar(str) ) score++
-        if ( containsNumber(str) ) score++
+        let score = passwordScoring(str)
         if (score <= 2) { // weak password
             pswStrengthBar.style.width = 'var(--password-strength-weak-width)'
             pswStrengthBar.style.backgroundColor = 'var(--password-strength-weak-color)'
@@ -66,11 +47,20 @@ pswInput.addEventListener(
 )
 
 function checkPswMatch() {
-    if (cfPswInput.value !== pswInput.value) pswMatchBar.style.display = 'none'
-    else pswMatchBar.style.display = 'block'
+    if (cfPswInput.value !== pswInput.value) pswMatchSymbol.style.visibility = 'hidden'
+    else pswMatchSymbol.style.visibility = 'visible'
 }
 
 cfPswInput.addEventListener(
     'input',
     () => checkPswMatch()
 )
+
+document.querySelector('#show-password').addEventListener('change', (e) => {
+    console.log(pswInput.type)
+    if (pswInput && cfPswInput && pswInput.type == "password") {
+        pswInput.type = cfPswInput.type = "text"
+    } else {
+        pswInput.type = cfPswInput.type = "password"
+    }
+})
