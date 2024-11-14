95ade96957e966a3934eaf15df705269590b48d6
Le Quoc Khoi <khoile903@gmail.com>
2024-11-15 00:00:04 +0700

    add: mark as read and delete screen

--- src/ActivityTags/script.js
+++ src/ActivityTags/script.js
@@ -2,11 +2,18 @@ const activity_tag = document.querySelectorAll(".activity-link-tag");
 const activity_follow_screen = document.querySelectorAll(".activity-follow");
 const see_more_button = document.querySelector(".see-more-svg");
 const navSettingContainer = document.querySelector(".nav-setting-container");
+const notificationButton = document.querySelector(".notification-svg");
 const like_button = document.querySelectorAll(
   ".activity-follow-infor-react.like"
 );
 
-console.log(like_button);
+// delete or mark read button
+const markOrDeleteContainer = document.querySelector(
+  ".activity-notification-block"
+);
+const deleteButton = document.querySelector(".delete-button");
+
+let followContainer;
 
 // change screen tag handle
 activity_tag.forEach((tag) => {
@@ -141,4 +148,59 @@ like_button.forEach((button) => {
       spanElement.classList.add("react-like-on");
     }
   });
+});
+
+//handle notificationButton
+
+notificationButton.addEventListener("click", (event) => {
+  const currentTag = event.currentTarget;
+  currentTag.classList.add("pop-up");
+  setTimeout(() => {
+    currentTag.classList.remove("pop-up");
+  }, 300);
+
+  const activeFollowScreens = document.querySelector(
+    ".activity-follow:not(.hidden)"
+  );
+  if (currentTag.classList.contains("active")) {
+    markOrDeleteContainer.classList.add("hidden");
+    currentTag.classList.remove("active");
+    followContainer = activeFollowScreens.querySelectorAll(
+      ".activity-follow-container"
+    );
+    followContainer.forEach((container) => {
+      container.classList.remove("grid-check-box");
+      container
+        .querySelector(".activity-follow-checkbox")
+        .classList.add("hidden");
+      const rightInfo = container.querySelector(".activity-follow-infor-right");
+      if (rightInfo != null) {
+        rightInfo.classList.remove("hidden");
+      } else {
+        container
+          .querySelector(".activity-follow-infor-header-name")
+          .classList.add("change-width");
+      }
+    });
+  } else {
+    markOrDeleteContainer.classList.remove("hidden");
+    currentTag.classList.add("active");
+    followContainer = activeFollowScreens.querySelectorAll(
+      ".activity-follow-container"
+    );
+    followContainer.forEach((container) => {
+      container.classList.add("grid-check-box");
+      container
+        .querySelector(".activity-follow-checkbox")
+        .classList.remove("hidden");
+      const rightInfo = container.querySelector(".activity-follow-infor-right");
+      if (rightInfo != null) {
+        rightInfo.classList.add("hidden");
+      } else {
+        container
+          .querySelector(".activity-follow-infor-header-name")
+          .classList.remove("change-width");
+      }
+    });
+  }
 });
