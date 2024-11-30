3d7236f635962f7bc3d53379bc174a9ee24862d3
ketamean <truongthanhtoan2003@gmail.com>
2024-12-01 00:17:29 +0700

    add metadata.js for views

--- src/metadata.js
+++ src/metadata.js
@@ -1,7 +1,19 @@
 const metadata = {}
 
-metadata.loginMetadata = {
+metadata.md_login = {
     'css': ['login.css'],
+}
+
+metadata.md_signup = {
+    'css': ['signup.css', 'password-input.css']
+}
+
+metadata.md_resetPassword = {
+    'css': ['reset-password.css', 'password-input.css']
+}
+
+metadata.md_feeds = {
+    'css': ['post.css', 'feeds.css']
 }
 
 module.exports = metadata
