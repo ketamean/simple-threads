d7e2ec4046d7ebd66ba661c65f6cc5a4dcf272ac
Le Quoc Khoi <khoile903@gmail.com>
2024-12-27 02:11:55 +0700

    Refactor: remove unused table and index creation queries from database configuration

--- src/config/database.js
+++ src/config/database.js
@@ -14,19 +14,4 @@ const client = new Client({
     .catch((err) => console.error("Connection error", err.stack));
 })();
 
-const createTablesQuery = require('./db/genQueryCreateTables');
-const createIndexesQuery = require('./db/genQueryCreateIndexes');
-
-(async() => {
-  try {
-    await client.query(createTablesQuery);
-    console.log("Tables created successfully");
-
-    await client.query(createIndexesQuery);
-    console.log("Indexes created successfully");
-  } catch (err) {
-    console.error("Error creating tables", err.stack);
-  }
-})();
-
 module.exports = client;
