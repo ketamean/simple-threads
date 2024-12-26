e8c1d5964656fced177a001329b1a62b58d99375
Sunflowerformylove <dodinhhai29904@gmail.com>
2024-12-26 23:10:59 +0700

    Merge branch 'main' of https://github.com/ketamean/simple-threads

--- src/models/threadsModel.js
+++ src/models/threadsModel.js
@@ -1,6 +1,7 @@
-// models/threadsModel.js
 const client = require("../config/database");
 
+// THIS DOES NOT HANDLE ERRORS.
+// ERRORS ARE HANDLED BY CONTROLLERS
 const thread = {
   createThread: async ({ user_id, content, createdAt }) => {
     const query = `
@@ -37,31 +38,32 @@ const thread = {
     const res = await client.query(query, [id]);
     return res.rows;
   },
-  getNLikes: async (id) => {
-    console.log("thread models: get n likes");
+  async getNLikes(id) {
+    console.log('thread models: get n likes')
     const query = `
       SELECT count(*)
       FROM Likes
       WHERE thread_id = $1;
     `;
     const values = [id];
-    const res = await client.query(query, values);
-    console.log(res);
-    return res;
+    const res = (await client.query(query, values)).rows[0];
+    console.log(res.count);
+    return res.count;
   },
-  getNComments: async (id) => {
-    console.log("thread models: get n comments");
+  async getNComments(id) {
+    console.log('thread models: get n comments')
     const query = `
       SELECT count(*)
       FROM Comments
       WHERE thread_id = $1;
     `;
     const values = [id];
-    const res = await client.query(query, values);
-    console.log(res);
-    return res;
+    const res = (await client.query(query, values)).rows[0];
+    console.log(res.count);
+    return res.count;
   },
-  checkUserLikedThread: async (threadId, userId) => {
+  async checkUserLikedThread(threadId, userId) {
+    console.log('check user liked')
     const query = `
       SELECT count(*)
       FROM Likes
@@ -69,26 +71,16 @@ const thread = {
     `;
     const values = [threadId, userId];
     const res = (await client.query(query, values)).rows;
-    if (rows.length > 1)
-      throw new Error(`User liked thread ${threadId} more than once`);
-    return rows.length === 1; // true if liked, false of have not liked yet
+    console.log(res.length);
+    if (res.length > 1) throw new Error(`User liked thread ${threadId} more than once`);
+    return res.length === 1; // true if liked, false of have not liked yet
   },
-  getThreadOwner: async (threadId) => {
+  async getThreadById(threadId, viewerId) {
+    const nLike = await this.getNLikes(threadId);
+    const nComments = await this.getNComments(threadId);
+    const liked = await this.checkUserLikedThread(threadId, viewerId);
     const query = `
-      SELECT u.username as 'username', u.id as 'userId', u.profile_picture as 'avatarImagePath', t.created_at as 'date', t.content as 'content'
-      FROM Threads t, Users u
-      WHERE t.id = $1 AND t.user_id = u.id;
-    `;
-    const res = (await client.query(query, [threadId])).rows[0];
-    if (liked) res.liked = true;
-    return res;
-  },
-  getThreadById: async (threadId, viewerId) => {
-    const nLike = this.getNLikes(threadId);
-    const nComments = this.getNComments(threadId);
-    const liked = this.checkUserLikedThread(threadId, viewerId);
-    const query = `
-      SELECT u.username as 'username', u.id as 'userId', u.profile_picture as 'avatarImagePath', t.created_at as 'date', t.content as 'content', $2 as 'nLikes', $3 as 'nComments'
+      SELECT u.username AS "username", u.id AS "userId", u.profile_picture AS "avatarImagePath", t.created_at AS "date", t.content AS "content", $2 AS "nLikes", $3 AS "nComments"
       FROM Threads t, Users u
       WHERE t.id = $1 AND t.user_id = u.id;
     `;
